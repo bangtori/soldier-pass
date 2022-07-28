bd3768c7b11cd75c8b97cbb183a41d1e78494134
Kyujeong <kyuj1102@gmail.com>
2022-07-29 01:24:40 +0900

    initProject

---
+++ SoldierPass/SoldierPass/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SoldierPass
+//
+//  Created by 권유정 on 2022/07/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
